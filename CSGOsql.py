2102d3a87317ac3026234c76566e66f345d14612
RyanMConstable <ryanmconstable@gmail.com>
2023-05-31 11:30:04 -0400

    Added DESC to the query so it sorts by newest instead of oldest

--- CSGOsql.py
+++ CSGOsql.py
@@ -58,7 +58,7 @@ def returnAllCodes():
 #This function takes in a steamid and returns the most recent match code from the table
 def findMostRecentGame(userid):
     #This is the SQL query to find the most recent game id
-    query = "SELECT gameid from gamestats WHERE steamid = {} order by date limit 1".format(userid)
+    query = "SELECT gameid from gamestats WHERE steamid = {} order by date DESC limit 1".format(userid)
     result = dbconnection.executeQuery(dbconnection.createConnection(), query)
     if result is None or result == []:
         return
