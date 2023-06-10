aefba2a425721642b6e402fdc5722f01d9be7ac5
RyanMConstable <ryanmconstable@gmail.com>
2023-06-09 22:21:18 -0400

    Remove testing prints from findMatchSteamAPI.py

--- findMatchSteamAPI.py
+++ findMatchSteamAPI.py
@@ -8,19 +8,15 @@ steamAPIKey = os.environ["STEAM_API_KEY"]
 
 #Return list of game codes since given code (include code given)
 def giveCodes(steamID, knownCode, steamidkey):
-    print(steamAPIKey, steamID, steamidkey)
     gamesList = []
     gamesList.append(knownCode)
     sharedUrl = 'https://api.steampowered.com/ICSGOPlayers_730/GetNextMatchSharingCode/v1?key={}&steamid={}&steamidkey={}&knowncode={}'.format(steamAPIKey, steamID, steamidkey, knownCode)
-    print(sharedUrl)
     r = requests.get(sharedUrl)
-    print("Starting status: " + str(r.status_code))
     #Create loop until status code is not 200
     #This will find every code until the newest, or it times out with 429 HTTP status code
     while r.status_code == 200:
         time.sleep(1/5)
         newCode = r.json()['result']['nextcode']
-        print(r.status_code, newCode)
         gamesList.append(newCode)
         sharedUrl = 'https://api.steampowered.com/ICSGOPlayers_730/GetNextMatchSharingCode/v1?key={}&steamid={}&steamidkey={}&knowncode={}'.format(steamAPIKey, steamID, steamidkey, newCode)
         r = requests.get(sharedUrl)
