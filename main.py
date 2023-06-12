db8555a4cb76094cd9ac1908f34a0ba010d665e2
RyanMConstable <ryanmconstable@gmail.com>
2023-06-12 14:02:02 -0400

    Added time calculator for updateAllUsers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #! C:\Users\ry4nm\AppData\Local\Programs\Python\Python311\python.exe
-import userCommands, CSGOsql
+import userCommands, CSGOsql, time
 
 
 if __name__ == '__main__':
@@ -8,5 +8,10 @@ if __name__ == '__main__':
     #Otherwise we want to automatically check for new games for every user
     CSGOsql.updateNewGames()
     
+    startTime = time.time()
     #!!!!!!This needs to be quicker!!!!!!
     CSGOsql.updateAllUsers()
+    
+    #Tracks time for CSGOsql.updateAllUsers()
+    totalTime = time.time()-startTime
+    print(f'Time: {totalTime:.2f} sec')
