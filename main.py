4d94850a9b20bea85222a50b18b1c7960953e2c6
RyanMConstable <ryanmconstable@gmail.com>
2023-06-12 03:04:54 -0400

    Change main to automatically update the tables

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #! C:\Users\ry4nm\AppData\Local\Programs\Python\Python311\python.exe
-import userCommands
+import userCommands, CSGOsql
 
 
 if __name__ == '__main__':
-    userCommands.mainChainCommand()
+    #userCommands.mainChainCommand() #This runs the admin functionalities
+    
+    #Otherwise we want to automatically check for new games for every user
+    CSGOsql.updateNewGames()
+    CSGOsql.updateAllUsers()
