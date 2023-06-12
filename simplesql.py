dc7c94aa72111412284d12747e16460c4bcc5806
RyanMConstable <ryanmconstable@gmail.com>
2023-06-12 13:44:55 -0400

    Removed old code from findMostRecentGame simplesql.py

--- simplesql.py
+++ simplesql.py
@@ -11,20 +11,3 @@ def findMostRecentGame(userid):
         return
     gameid = result[0][0]
     return gameid
-    
-    
-    #OLD CODE BELOW
-    #This is the SQL query to find the most recent game id
-    query = "SELECT gameid from gamestats WHERE steamid = {} order by date DESC limit 1".format(userid)
-    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
-    if result is None or result == []:
-        return
-    gameid = result[0][0]
-    
-    #Now we use the gameid to find the matchcode from the first table
-    query = "SELECT code from gamecodes WHERE id = {}".format(gameid)
-    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
-    if result is None or result == []:
-        return
-    #Returns match code of the most recent game
-    return result[0][0]
