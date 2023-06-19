caa191dfb3d06f9a743a105753c44977cc429797
RyanMConstable <ryanmconstable@gmail.com>
2023-06-19 18:09:56 -0400

    Added check for newgames list being longer than 1

--- downloadDemsMain.py
+++ downloadDemsMain.py
@@ -1,5 +1,5 @@
 import findMatchSteamAPI as API
-import CSGOsql, getJSONInfo, os
+import CSGOsql, getJSONInfo, os, time
 #1) Given a userid, and steamkey, find that users new list of codes
 #2) Generate every users new games
 #3) Update the users recentgamecode with the newest game code
@@ -8,7 +8,8 @@ import CSGOsql, getJSONInfo, os
 
 #Update function for new users
 CSGOsql.updateNewGames()
-print("Ending updateNewGames")
+print("Ending updateNewGames\n")
+
 
 #Set variables early so time isn't wasted in the loop
 codesIngamecodes = CSGOsql.findAllCodes
@@ -17,6 +18,14 @@ codesInGamestats = CSGOsql.findAllCodesInStats
 #For every user in the user db, find the new codes and then download new ones
 for user in CSGOsql.findAllid():
     updateList = API.generateNewCodes(user[0], user[1])
+    
+    #Set new update
+    if len(updateList) > 1:
+        CSGOsql.newRecentGame(user[0], updateList[-1])
+    else:
+        print("Already up to date\n")
+        continue
+    
     if any(updateList):
         for code in updateList:
             #Checks to see if the code is in the database
@@ -26,3 +35,5 @@ for user in CSGOsql.findAllid():
             if (code in os.listdir(os.path.join(os.getcwd(), 'demoDownloads'))):
                 continue
             getJSONInfo.downloadDems(code)
+            
+
