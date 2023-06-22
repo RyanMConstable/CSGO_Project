9e44834e16a369e32e7e640fc33e25fefe7cbc94
RyanMConstable <ryanmconstable@gmail.com>
2023-06-22 17:06:35 -0400

    Added back the loop so it permanently runs and checks for new downloads every 30 seconds

--- analyzeDemsMain.py
+++ analyzeDemsMain.py
@@ -1,38 +1,41 @@
 from multiprocessing import Pool
-import getJSONInfo, os, CSGOsql
+import getJSONInfo, os, CSGOsql, time
 
 
 #Run this script every 30 seconds or so to check for new games, increase if load increases
 if __name__ == '__main__':
-    #Sets the number of processes it should run, max is 60 on windows, so the max will be set to 40 just in case
-    processes = len(os.listdir(os.path.join(os.getcwd(), 'demoDownloads')))
-    if processes <= 0:
-        print("No demos in the demoDownloads directory")
-        exit(1)
-    elif processes > 40:
-        print("Processes set to 40")
-        processes = 40
-    
-    
-    #This way they are not called multiple times, increases speed of program
-    gamesIngamecodes = CSGOsql.findAllCodes()
-    gamesIngamestats = CSGOsql.findAllCodesInStats()
-    
-    #Multiprocesses demoDownloads to speed up analyzing
-    with Pool(processes) as p:
-        x = p.map(getJSONInfo.analyzeDem, os.listdir(os.path.join(os.getcwd(), 'demoDownloads')))
-        #X is going to be a list of the gamecode at index 0 and the parsed info in index 1
-        #Call functions to add them to the database below
-        for game in x:
-            if game[0] in gamesIngamecodes:
-                print("Game is in gamecodes already")
-                if game[0] in gamesIngamestats:
-                    print("Game is also in gamestats")
+    while True:
+        #Sets the number of processes it should run, max is 60 on windows, so the max will be set to 40 just in case
+        processes = len(os.listdir(os.path.join(os.getcwd(), 'demoDownloads')))
+        if processes <= 0:
+            print("No demos in the demoDownloads directory")
+            print("Waiting")
+            time.sleep(30)
+        elif processes > 40:
+            print("Processes set to 40")
+            processes = 40
+        
+        
+        #This way they are not called multiple times, increases speed of program
+        gamesIngamecodes = CSGOsql.findAllCodes()
+        gamesIngamestats = CSGOsql.findAllCodesInStats()
+        
+        #Multiprocesses demoDownloads to speed up analyzing
+        with Pool(processes) as p:
+            x = p.map(getJSONInfo.analyzeDem, os.listdir(os.path.join(os.getcwd(), 'demoDownloads')))
+            #X is going to be a list of the gamecode at index 0 and the parsed info in index 1
+            #Call functions to add them to the database below
+            for game in x:
+                if game[0] in gamesIngamecodes:
+                    print("Game is in gamecodes already")
+                    if game[0] in gamesIngamestats:
+                        print("Game is also in gamestats")
+                    else:
+                        CSGOsql.addGameStats(game)
+                        print("Game is not yet in gamestats")
                 else:
+                    print("Game is being added to gamecodes")
+                    CSGOsql.addGameCodes([game[0]])
                     CSGOsql.addGameStats(game)
-                    print("Game is not yet in gamestats")
-            else:
-                print("Game is being added to gamecodes")
-                CSGOsql.addGameCodes([game[0]])
-                CSGOsql.addGameStats(game)
-    exit(0)
+        print("Waiting")
+        time.sleep(30)
