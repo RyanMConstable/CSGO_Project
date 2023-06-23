7913eef06e45e1476c03b56852e2de513c2f0730
RyanMConstable <ryanmconstable@gmail.com>
2023-06-22 23:12:06 -0400

    Changed downloadDemsMain.py to logging system

--- downloadDemsMain.py
+++ downloadDemsMain.py
@@ -8,7 +8,7 @@ import CSGOsql, getJSONInfo, os, time
 if __name__ == '__main__':
     #Update function for new users
     CSGOsql.updateNewGames()
-    print("Ending updateNewGames\n")
+    os.system("echo [INFO] Ending updateNewGames >> downloaddemoLOG.txt")
 
     #Variable to track amount of time
     startTime = time.time()
@@ -19,14 +19,12 @@ if __name__ == '__main__':
 
     #For every user in the user db, find the new codes and then download new ones
     for user in CSGOsql.findAllid():
-        print('+-----------------+')
-        print('|' + str(user[0]) + '|')
-        print('+-----------------+')
+        os.system("echo [INFO] User: {} >> downloaddemoLOG.txt".format(str(user[0])))
         updateList = API.generateNewCodes(user[0], user[1])
         
         #If there are new codes update the recentgame code and download the demo
         if any(updateList):
-            print("Updating: " + str(updateList))
+            os.system("echo [INFO] Updating: {} >> downloaddemoLOG.txt".format(str(updateList)))
             CSGOsql.newRecentGame(user[0], updateList[-1])
             for code in updateList:
                 #Checks to see if the code is in the database
@@ -37,10 +35,10 @@ if __name__ == '__main__':
                     continue
                 getJSONInfo.downloadDems(code)
         else:
-            print("Already up to date\n")
+            os.system("echo [INFO] Already up to date >> downloaddemoLOG.txt")
             continue
 
     #Prints the amount of time to download all the user files     
     totalTime = time.time()-startTime
-    print(f'Time: {totalTime:.2f} sec')
-    time.sleep(30)
+    os.system("echo [EXIT] Exiting after: {} >> downloaddemoLOG.txt".format(f'Time: {totalTime:.2f} sec'))
+    exit(0)
