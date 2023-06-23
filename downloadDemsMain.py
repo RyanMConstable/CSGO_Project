42b9f9793f0ccc1bfa1e476022a6c7c17e89c0de
RyanMConstable <ryanmconstable@gmail.com>
2023-06-23 17:55:48 -0400

    Updated downloaddems logs

--- downloadDemsMain.py
+++ downloadDemsMain.py
@@ -8,7 +8,6 @@ import CSGOsql, getJSONInfo, os, time
 if __name__ == '__main__':
     #Update function for new users
     CSGOsql.updateNewGames()
-    os.system("echo [INFO] Ending updateNewGames >> autoLOG.txt")
 
     #Variable to track amount of time
     startTime = time.time()
@@ -19,13 +18,11 @@ if __name__ == '__main__':
 
     #For every user in the user db, find the new codes and then download new ones
     for user in CSGOsql.findAllid():
-        os.system("echo [INFO] User: {} >> autoLOG.txt".format(str(user[0])))
         updateList = API.generateNewCodes(user[0], user[1])
         #updateList = ['CSGO-65zTr-n7ATe-MsdXS-PDo78-DBF4C', 'CSGO-ZvdK8-iqdX7-V4BvA-EBQfh-GusnC']
         
         #If there are new codes update the recentgame code and download the demo
         if any(updateList):
-            os.system("echo [INFO] Updating: {} >> autoLOG.txt".format(str(updateList)))
             CSGOsql.newRecentGame(user[0], updateList[-1])
             for code in updateList:
                 #Checks to see if the code is in the database
@@ -36,13 +33,12 @@ if __name__ == '__main__':
                     continue
                 getJSONInfo.downloadDems(code)
         else:
-            os.system("echo [INFO] Already up to date >> autoLOG.txt")
             continue
     
     
     #Call analyze
     os.system("echo [CALL] Starting Analyze File >> autoLOG.txt")
-    #os.system("python {}".format(os.path.join(os.getcwd(), "analyzeDemsMain.py")))
+    os.system("python {}".format(os.path.join(os.getcwd(), "analyzeDemsMain.py")))
     os.system("echo [CALL] Ending Analyze File >> autoLOG.txt")
     
     #Prints the amount of time to download all the user files     
