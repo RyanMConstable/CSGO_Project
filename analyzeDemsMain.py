29cc1679706cb49c93426621fd041596e4a47a72
RyanMConstable <ryanmconstable@gmail.com>
2023-06-23 01:15:23 -0400

    Changed file to autoLOG

--- analyzeDemsMain.py
+++ analyzeDemsMain.py
@@ -5,44 +5,47 @@ import getJSONInfo, os, CSGOsql, time
 
 #Run this script every 30 seconds or so to check for new games, increase if load increases
 if __name__ == '__main__':
-    os.system("echo [START] Starting at time: {} >> analyzeLOG.txt".format(datetime.now()))
+    os.system("echo [START] Starting at time: {} >> autoLOG.txt".format(datetime.now()))
     #Sets the number of processes it should run, max is 60 on windows, so the max will be set to 40 just in case
     processes = len(os.listdir(os.path.join(os.getcwd(), 'demoDownloads')))
     if processes <= 0:
-        os.system("echo [INFO] No demos in the demoDownloads directory >> analyzeLOG.txt")
-        os.system("echo [EXIT] Exiting >> analyzeLOG.txt")
+        os.system("echo [INFO] No demos in the demoDownloads directory >> autoLOG.txt")
+        os.system("echo [EXIT] Exiting >> autoLOG.txt")
         exit(0)
     elif processes > 40:
-        os.system("echo [INFO] Processes set to 40 >> analyzeLOG.txt".format())
+        os.system("echo [INFO] Processes set to 40 >> autoLOG.txt".format())
         processes = 40
     else:
-        os.system("echo [DIRECTORIES] {} >> analyzeLOG.txt".format(os.listdir(os.path.join(os.getcwd(), 'demoDownloads'))))
+        os.system("echo [DIRECTORIES] {} >> autoLOG.txt".format(os.listdir(os.path.join(os.getcwd(), 'demoDownloads'))))
     
     
     #This way they are not called multiple times, increases speed of program
     gamesIngamecodes = CSGOsql.findAllCodes()
     gamesIngamestats = CSGOsql.findAllCodesInStats()
+    os.system("echo [DEBUG] {} >> autoLOG.txt".format(gamesIngamecodes))
+    os.system("echo [DEBUG] {} >> autoLOG.txt".format(gamesIngamestats))
+    
     
     #Multiprocesses demoDownloads to speed up analyzing
-    os.system("echo [SET1] >> analyzeLOG.txt")
+    os.system("echo [SET1] >> autoLOG.txt")
     with Pool(processes) as p:
-        os.system("echo [SET2] >> analyzeLOG.txt")
+        os.system("echo [SET2] >> autoLOG.txt")
         x = p.map(getJSONInfo.analyzeDem, os.listdir(os.path.join(os.getcwd(), 'demoDownloads')))
-        os.system("echo [SET3] >> analyzeLOG.txt")
+        os.system("echo [SET3] {} >> autoLOG.txt".format(x))
         #X is going to be a list of the gamecode at index 0 and the parsed info in index 1
         #Call functions to add them to the database below
         for game in x:
-            os.system("echo [GAME?] {} >> analyzeLOG.txt".format(game))
+            os.system("echo [GAME?] {} >> autoLOG.txt".format(game))
             if game[0] in gamesIngamecodes:
-                os.system("echo [INFO] Game is in gamecodes already >> analyzeLOG.txt")
+                os.system("echo [INFO] Game is in gamecodes already >> autoLOG.txt")
                 if game[0] in gamesIngamestats:
-                    os.system("echo [INFO] Game is also in gamestats >> analyzeLOG.txt")
+                    os.system("echo [INFO] Game is also in gamestats >> autoLOG.txt")
                 else:
                     CSGOsql.addGameStats(game)
-                    os.system("echo [ADD] Adding to gamestats >> analyzeLOG.txt")
+                    os.system("echo [ADD] Adding to gamestats >> autoLOG.txt")
             else:
-                os.system("echo [DOUBLEADD] Game is being added to gamecodes and gamestats >> analyzeLOG.txt")
+                os.system("echo [DOUBLEADD] Game is being added to gamecodes and gamestats >> autoLOG.txt")
                 CSGOsql.addGameCodes([game[0]])
                 CSGOsql.addGameStats(game)
-    os.system("echo [EXIT] Exiting >> analyzeLOG.txt")
+    os.system("echo [EXIT] Exiting >> autoLOG.txt")
     exit(0)
