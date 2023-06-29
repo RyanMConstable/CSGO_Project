850901354580621b4b9317e3ca2eea84282b25d4
RyanMConstable <ryanmconstable@gmail.com>
2023-06-28 21:02:13 -0400

    Removed changing directories from downloading dem (problem with multiprocessing I beleive)

--- getJSONInfo.py
+++ getJSONInfo.py
@@ -155,24 +155,20 @@ def returnGameInfo(jsonInputFormat):
 ###############################################################
 
 
-#This function downloads a game code
+#This function downloads a game code, only if the game is not in the folder already
 def downloadDems(code):
     #Keep originalDir in case of adding new
-    originalDir = os.getcwd()
-    os.chdir("demoDownloads")
-    if code not in os.listdir():
-        os.mkdir(code)
-        os.chdir(code)
-    else:
-        os.chdir(code)
-    #Here we have a new directory with the name of the code, inside we want to just download the game
-    os.system("csgodm download {} --output {}".format(code, os.getcwd()))
-    os.chdir(originalDir)
-    #Here we want to see the length of the files in the directory that was created
-    downloadPath = os.path.join(os.path.join(os.getcwd(), 'demoDownloads'), code)
-    if len(os.listdir(downloadPath)) == 0:
-        os.system("rd /s /q {}".format(downloadPath))
+    downloadDir = os.path.join(os.getcwd(), 'demoDownloads')
+    downloadCodeDir = os.path.join(downloadDir, code)
+    if code not in os.listdir(downloadDir):
+        os.mkdir(downloadCodeDir)
+        #Here we have a new directory with the name of the code, inside we want to just download the game
+        os.system("csgodm download {} --output {}".format(code, downloadCodeDir))
+        #Here we want to see the length of the files in the directory that was created
+        if len(os.listdir(downloadCodeDir)) == 0:
+            os.system("rd /s /q {}".format(downloadCodeDir))
     return
+
 
 #New analyzeDem function... Outputs the same code and returnParse function but is able to be done with multiprocessing
 #Returns none if there is a weird error...
