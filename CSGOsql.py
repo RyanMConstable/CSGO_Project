1e308253af9080baea6e489392189d63861098be
RyanMConstable <ryanmconstable@gmail.com>
2023-07-03 13:40:41 -0400

    Patched SQL return that would return null instead of the minimum values -bottom

--- CSGOsql.py
+++ CSGOsql.py
@@ -532,6 +532,6 @@ def redownload():
 
 #Function to find the bottom X Users
 def findBottom(category, limit):
-    query = "SELECT name, {} FROM gamestats ORDER BY {} ASC LIMIT {}".format(category, category, limit)
+    query = "SELECT name, {} FROM gamestats WHERE {} > 0.0 ORDER BY {} ASC LIMIT {}".format(category, category, category, limit)
     result = dbconnection.executeQuery(dbconnection.createConnection(), query)
     return result
