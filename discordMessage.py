8e39b2d7ceda51dd9d3e462165082522b671b6c9
RyanMConstable <ryanmconstable@gmail.com>
2023-07-20 18:39:48 -0400

    Added notifications for discord through tokens

--- discordMessage.py
+++ discordMessage.py
@@ -2,4 +2,11 @@ import requests, os
 #This file should notify the discord that the new game has been added... for now
 
 def notify():
+    payload = {
+        'content' : "Game Added"
+    }
+    header = {
+        'Authorization' : F'Bot {os.environ["DISCORD_TOKEN"]}'
+    } 
+    requests.post("https://discord.com/api/v9/channels/1105249488702030026/messages", data=payload, headers = header)
     return
