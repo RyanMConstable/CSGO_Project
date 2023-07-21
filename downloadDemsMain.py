7b40ea62256b60fa8bd83bad90dd7f5e4330735d
RyanMConstable <ryanmconstable@gmail.com>
2023-07-20 23:13:38 -0400

    Added logging import

--- downloadDemsMain.py
+++ downloadDemsMain.py
@@ -1,5 +1,5 @@
 import findMatchSteamAPI as API
-import CSGOsql, getJSONInfo, os, subprocess
+import CSGOsql, getJSONInfo, os, subprocess, logging
 #1) Given a userid, and steamkey, find that users new list of codes
 #2) Generate every users new games
 #3) Update the users recentgamecode with the newest game code
