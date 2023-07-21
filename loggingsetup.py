9d267effc381bcde5a6b267a77bef40708e34d40
RyanMConstable <ryanmconstable@gmail.com>
2023-07-21 13:34:46 -0400

    Added logsetup where we can add as many logs as we want

--- loggingsetup.py
+++ loggingsetup.py
@@ -12,3 +12,16 @@ def setup_logger(logger_name, log_file, level=logging.INFO):
     log_setup.setLevel(level)
     log_setup.addHandler(fileHandler)
     log_setup.addHandler(streamHandler)
+    return
+
+#return dictionary of logging objects  
+def logsetup():
+    setup_logger('auto_log', 'auto.log')
+    autolog = logging.getLogger('auto_log')
+    setup_logger('add_log', 'auto.log')
+    addlog = logging.getLogger('add_log')
+    logdict = {}
+    logdict['auto'] = autolog
+    logdict['add'] = addlog
+    return logdict
+    
