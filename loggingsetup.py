5bb2c2e9dc31b4aef572e632f715c494a4b0e88f
RyanMConstable <ryanmconstable@gmail.com>
2023-07-21 18:13:37 -0400

    Changed logging functions

--- loggingsetup.py
+++ loggingsetup.py
@@ -1,17 +1,21 @@
 import logging
-autolog = logging.getLogger('auto_log')
-addlog = logging.getLogger('add_log')
 
 #For autolog
-autolog.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(levelname)s: %(asctime)s %(message)s', datefmt=r'%m/%d/%Y %I:%M:%S %p')
-fileHandler = logging.FileHandler("auto.log", mode = "a+")
-fileHandler.setFormatter(formatter)
-autolog.addHandler(fileHandler)
+def autologf():
+    autolog = logging.getLogger('auto_log')
+    autolog.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(levelname)s: %(asctime)s %(message)s', datefmt=r'%m/%d/%Y %I:%M:%S %p')
+    fileHandler = logging.FileHandler("auto.log", mode = "a+")
+    fileHandler.setFormatter(formatter)
+    autolog.addHandler(fileHandler)
+    return
 
 #For addlog
-addlog.setLevel(logging.INFO)
-formatter = logging.Formatter('%(levelname)s: %(asctime)s %(message)s', datefmt=r'%m/%d/%Y %I:%M:%S %p')
-fileHandler = logging.FileHandler("add.log", mode = "a+")
-fileHandler.setFormatter(formatter)
-addlog.addHandler(fileHandler)
+def addlogf():
+    addlog = logging.getLogger('add_log')
+    addlog.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(levelname)s: %(asctime)s %(message)s', datefmt=r'%m/%d/%Y %I:%M:%S %p')
+    fileHandler = logging.FileHandler("add.log", mode = "a+")
+    fileHandler.setFormatter(formatter)
+    addlog.addHandler(fileHandler)
+    return
