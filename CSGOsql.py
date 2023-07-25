0826616fe5d1fff489e01eb87d20933625fbc2a5
RyanMConstable <ryanmconstable@gmail.com>
2023-07-24 21:46:28 -0400

    Moved large amount of stat functions

--- CSGOsql.py
+++ CSGOsql.py
@@ -174,6 +174,65 @@ def findTopUser(category, userid, limit):
     head = ["Name", category]
     return [result, head]
 
+#Very useful stat function, type in the column name of the database, and the steam id and it will return the sum of that column
+def selectCombinedUserStat(stat, steamid):
+    query = "SELECT SUM({}) FROM gamestats WHERE steamid = '{}'".format(stat, steamid)
+    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
+    if result is None or result == []:
+        return
+    return result[0][0]
+
+
+#Very useful stat function, type in the column name of the database, and the steam id and it will return the avg of that column
+def selectAvgUserStat(stat, steamid, limiter):
+    query = "SELECT AVG({}) FROM (SELECT {} FROM gamestats WHERE steamid = '{}' ORDER BY date DESC LIMIT {}) as test".format(stat, stat, steamid, limiter)
+    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
+    if result is None or result == []:
+        return
+    return result[0][0]
+
+#Very useful stat function, type in the column name of the database, and the steam id and it will return the sum of that column
+def selectSumUserStat(stat, steamid, limiter):
+    query = "SELECT SUM({}) FROM (SELECT {} FROM gamestats WHERE steamid = '{}' ORDER BY date DESC LIMIT {}) as test".format(stat, stat, steamid, limiter)
+    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
+    if result is None or result == []:
+        return
+    return result[0][0]
+
+
+#Function to find how many games the user has within the table
+def findNumberOfGames(steamid):
+    query = "SELECT COUNT(*) FROM gamestats WHERE steamid = '{}'".format(steamid)
+    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
+    if result is None or result == []:
+        return
+    return result[0][0]
+
+
+#Function to find all rows for a given user in the table
+def returnAllUserRows(steamid):
+    query = "SELECT name, totalkills, score, tk_count, assist, deaths, 5k, 4k, 3k, 2k, 1k, headshot, kd, rws, shot_count, hit_count, flashbang_thrown, smoke_thrown, he_thrown, incendiary_thrown, decoy_thrown, round_count FROM gamestats WHERE steamid = '{}'".format(steamid)
+    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
+    if result is None or result == []:
+        return
+    return result
+
+#function to find the top 1 user
+def findTop1user(category, userid):
+    query = "SELECT name, {} FROM gamestats WHERE steamid = {} ORDER BY {} DESC LIMIT 1".format(category, userid, category)
+    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
+    if result is None or result == []:
+        return
+    return result[0][1]
+
+
+#Function to find user and stat
+def finduserandstat(category):
+    query = "SELECT name, {} FROM gamestats ORDER BY {} DESC LIMIT 1".format(category, category)
+    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
+    if result is None or result == []:
+        return
+    return str(result[0][0]) + ": " + str(result[0][1])
 
 ##################################################################################
 #################     END OF STAT FUNCTIONS             ##########################
@@ -389,65 +448,7 @@ def findAllid():
 #########################Finding Game Stats#################################
 ############################################################################
 
-#Very useful stat function, type in the column name of the database, and the steam id and it will return the sum of that column
-def selectCombinedUserStat(stat, steamid):
-    query = "SELECT SUM({}) FROM gamestats WHERE steamid = '{}'".format(stat, steamid)
-    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
-    if result is None or result == []:
-        return
-    return result[0][0]
 
-
-#Very useful stat function, type in the column name of the database, and the steam id and it will return the avg of that column
-def selectAvgUserStat(stat, steamid, limiter):
-    query = "SELECT AVG({}) FROM (SELECT {} FROM gamestats WHERE steamid = '{}' ORDER BY date DESC LIMIT {}) as test".format(stat, stat, steamid, limiter)
-    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
-    if result is None or result == []:
-        return
-    return result[0][0]
-
-#Very useful stat function, type in the column name of the database, and the steam id and it will return the sum of that column
-def selectSumUserStat(stat, steamid, limiter):
-    query = "SELECT SUM({}) FROM (SELECT {} FROM gamestats WHERE steamid = '{}' ORDER BY date DESC LIMIT {}) as test".format(stat, stat, steamid, limiter)
-    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
-    if result is None or result == []:
-        return
-    return result[0][0]
-
-
-#Function to find how many games the user has within the table
-def findNumberOfGames(steamid):
-    query = "SELECT COUNT(*) FROM gamestats WHERE steamid = '{}'".format(steamid)
-    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
-    if result is None or result == []:
-        return
-    return result[0][0]
-
-
-#Function to find all rows for a given user in the table
-def returnAllUserRows(steamid):
-    query = "SELECT name, totalkills, score, tk_count, assist, deaths, 5k, 4k, 3k, 2k, 1k, headshot, kd, rws, shot_count, hit_count, flashbang_thrown, smoke_thrown, he_thrown, incendiary_thrown, decoy_thrown, round_count FROM gamestats WHERE steamid = '{}'".format(steamid)
-    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
-    if result is None or result == []:
-        return
-    return result
-
-#function to find the top 1 user
-def findTop1user(category, userid):
-    query = "SELECT name, {} FROM gamestats WHERE steamid = {} ORDER BY {} DESC LIMIT 1".format(category, userid, category)
-    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
-    if result is None or result == []:
-        return
-    return result[0][1]
-
-
-#Function to find user and stat
-def finduserandstat(category):
-    query = "SELECT name, {} FROM gamestats ORDER BY {} DESC LIMIT 1".format(category, category)
-    result = dbconnection.executeQuery(dbconnection.createConnection(), query)
-    if result is None or result == []:
-        return
-    return str(result[0][0]) + ": " + str(result[0][1])
 
 
 
