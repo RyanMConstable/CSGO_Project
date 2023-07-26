0bf59c8d3901c6520dc78baabe1dedaf4cf86dd7
RyanMConstable <ryanmconstable@gmail.com>
2023-07-26 17:33:50 -0400

    Commented out test discord

--- discordMessage.py
+++ discordMessage.py
@@ -8,6 +8,9 @@ def notify():
     header = {
         'Authorization' : F'Bot {os.environ["DISCORD_TOKEN"]}'
     } 
-    requests.post("https://discord.com/api/v9/channels/1105249488702030026/messages", data=payload, headers = header)
+    #Test discord directly below
+    #requests.post("https://discord.com/api/v9/channels/1105249488702030026/messages", data=payload, headers = header)
+    
+    #Our main discord
     requests.post("https://discord.com/api/v9/channels/1091583699746836511/messages", data=payload, headers = header)
     return
