bdbd6cd3294a06f2f40529a49a2b0521084a4e78
RyanMConstable <ryanmconstable@gmail.com>
2023-07-26 17:37:01 -0400

    Updated what the notify function should do

--- discordMessage.py
+++ discordMessage.py
@@ -1,6 +1,19 @@
 import requests, os
 #This file should notify the discord that the new game has been added... for now
 
+
+
+#In here we want to actually find what discords this should be sent to...
+#We might want to send directly to users after a game?
+
+
+#1 find all servers that the bot is in
+#2 find all servers that the user is in
+    
+#3 If there is a text channel in a server that the user and the bot are both in
+#4 If there is a channel with "bot" in the name, send it there, otherwise send to random 1 channel
+
+
 def notify():
     payload = {
         'content' : "-Summary Time"
