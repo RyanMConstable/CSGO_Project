e85767f7b7d6950076467907b4f38e36fffbf9e8
RyanMConstable <ryanmconstable@gmail.com>
2023-07-31 14:47:19 -0400

    Changed query to F instead of .format

--- CSGOsql.py
+++ CSGOsql.py
@@ -351,7 +351,7 @@ def finduserandstat(category):
 
 #Very useful stat function, type in the column name of the database, and the steam id and it will return the sum of that column
 def selectCombinedUserStat(stat, steamid):
-    query = "SELECT SUM({}) FROM gamestats WHERE steamid = '{}'".format(stat, steamid)
+    query = F"SELECT SUM({stat}) FROM gamestats WHERE steamid = '{steamid}'"
     result = dbconnection.executeQuery(dbconnection.createConnection(), query)
     if result is None or result == []:
         return
