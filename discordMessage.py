e684e06d84850299c6afc915d2141b6b37e31e2e
RyanMConstable <ryanmconstable@gmail.com>
2023-08-06 17:40:43 -0400

    Removed testing channel from discordmessage

--- discordMessage.py
+++ discordMessage.py
@@ -21,9 +21,5 @@ def notify(game = None):
     header = {
         'Authorization' : F'Bot {os.environ["DISCORD_TOKEN"]}'
     } 
-    #Test discord directly below
-    #requests.post("https://discord.com/api/v9/channels/1105249488702030026/messages", data=payload, headers = header)
-    
-    #Our main discord
     requests.post("https://discord.com/api/v9/channels/1091583699746836511/messages", data=payload, headers = header)
     return
