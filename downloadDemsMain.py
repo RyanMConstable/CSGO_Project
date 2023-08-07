bf9a7195c243bf5b5a3851fc6930f885892623ac
RyanMConstable <ryanmconstable@gmail.com>
2023-08-07 18:33:10 -0400

    Reminder to revamp documentation

--- downloadDemsMain.py
+++ downloadDemsMain.py
@@ -2,11 +2,8 @@ import findMatchSteamAPI as API
 import CSGOsql, getJSONInfo, os, subprocess
 from loggingsetup import autologf
 
-#1) Given a userid, and steamkey, find that users new list of codes
-#2) Generate every users new games
-#3) Update the users recentgamecode with the newest game code
-#4) Check the list 
 
+#Create fine documentation for each file
 if __name__ == '__main__':
     autolog = autologf()
     #Update function for new users (IE users who have a row in the user table, but not the recentgame table)
