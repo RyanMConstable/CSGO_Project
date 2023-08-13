53f5a859e7d17cdb9272cbc738636a45ac64d0e9
ryanmconstable <ryanmconstable@gmail.com>
2023-08-12 21:25:11 -0400

    Removed comment

--- downloadDemsMain.py
+++ downloadDemsMain.py
@@ -44,6 +44,6 @@ if __name__ == '__main__':
     
 
     #Call analyze only if there are directories in demoDownloads (TODO)
-    #Calls up to 10 times, if it calls more than twice there is most likely an issue (enable logging at that point)
+    #Calls up to 10 times, if it calls more than twice there is most likely an issue
     subprocess.call(["python", os.path.join(os.getcwd(), "analyzeDemsMain.py")])
     exit(0)
