c85a23e567f25f133197fe61ea2b82539e98e98f
RyanMConstable <ryanmconstable@gmail.com>
2023-08-14 11:42:42 -0400

    Changed simplesql to new table :

--- simplesql.py
+++ simplesql.py
@@ -4,7 +4,7 @@ import dbconnection
 
 #This function takes in a steamid and returns the most recent match code from the table
 def findMostRecentGame(userid):
-    query = "SELECT code FROM recentgame WHERE steamid = '{}'".format(userid)
+    query = "SELECT gamecode FROM discorduser WHERE steamid = '{}'".format(userid)
     result = dbconnection.executeQuery(dbconnection.createConnection(), query)
     if any(result):
         return result[0][0]
