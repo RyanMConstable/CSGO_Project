cecba26cacd79a586a518edc7aecff0c7933c29b
RyanMConstable <ryanmconstable@gmail.com>
2023-08-18 13:10:15 -0400

    Changed findgameinfo for summary time

--- CSGOsql.py
+++ CSGOsql.py
@@ -287,7 +287,7 @@ def findGameInfo(gameid = None):
             return
         gameid = result[0][0]
     #Here we want to find all the game info
-    query = F"SELECT name, adr, team_damage, tk_count FROM gamestats WHERE gameid = {gameid}"
+    query = F"SELECT name, adr, team_damage, tk_count, teamflashcount, enemyflashcount, teamflashduration, enemyflashduration FROM gamestats WHERE gameid = {gameid}"
     result = dbconnection.executeQuery(dbconnection.createConnection(), query)
     return result
 
